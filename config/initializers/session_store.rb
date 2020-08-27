70ec93f513d25c2dbd6b6d4dbfe2babce7e4c462
MaryAnn <annychuks07@gmail.com>
2020-08-27 18:34:54 +0100

    fix bug in session domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "https://enigmatic-retreat-81755.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: :all
 
 else
   Rails.application.config.session_store :cookie_store, key: "_tracker_app"
