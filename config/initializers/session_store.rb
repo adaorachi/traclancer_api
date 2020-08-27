a4912fd9836e2ef58dc7dd470439f991420baa48
MaryAnn <annychuks07@gmail.com>
2020-08-27 20:02:21 +0100

    fix session store config-3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,2 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "enigmatic-retreat-81755.herokuapp.com/api/v1"
+Rails.application.config.session_store :cookie_store, :key => '_tracker_app', :domain=>:all
 
-else
-  Rails.application.config.session_store :cookie_store, key: "_tracker_app"
-end
