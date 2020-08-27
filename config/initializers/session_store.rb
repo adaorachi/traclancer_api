5cc51fab3ebd1216321f0778ae5f7d16007857eb
MaryAnn <annychuks07@gmail.com>
2020-08-27 17:48:03 +0100

    fix routing path in initializers -2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "enigmatic-retreat-81755.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "https://enigmatic-retreat-81755.herokuapp.com"
 
 else
   Rails.application.config.session_store :cookie_store, key: "_tracker_app"
