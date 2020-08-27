44f985eba9fb32ea4e9230c55c41a6e455b064ff
MaryAnn <annychuks07@gmail.com>
2020-08-27 11:39:05 +0100

    fix session store route path

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "tracker-app.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "blooming-refuge-37740.herokuapp.com"
+
 else
   Rails.application.config.session_store :cookie_store, key: "_tracker_app"
 end
