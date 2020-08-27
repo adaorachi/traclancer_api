e3def46aa8621baa4c8b677ba244e2141fef985b
MaryAnn <annychuks07@gmail.com>
2020-08-27 14:19:34 +0100

    adjust seed data

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
   belongs_to :project_category
   belongs_to :owned_user, class_name: 'User'
-  belongs_to :claimed_user, class_name: 'User'
+  # belongs_to :claimed_user, class_name: 'User'
 
   has_many :project_stages
   has_many :project_milestones
@@ -14,5 +16,4 @@ class Project < ApplicationRecord
   scope :completed_projects, ->(current_user) { where('claimed_user_id = ? and claimed = ? and completed = ?', current_user, true, true) }
   scope :created_projects, ->(current_user) { where('owned_user_id = ? and claimed = ? and completed = ?', current_user, false, false) }
   scope :available_projects, ->(project_slug) { where('slug = ? and completed = ? and claimed = ?', project_slug, false, false) }
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,12 +9,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 10.times do |n|
-  enum = ['freelancer', 'freelancer', 'freelancer', 'freelancer', 'freelancer', 'freelancer', 'freelancer', 'freelancer', 'client', 'client', 'client']
+  enum = %w[freelancer freelancer freelancer freelancer freelancer freelancer freelancer client client client client]
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   username = Faker::Internet.user_name
   status = enum[n]
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n + 1}@railstutorial.org"
   password = 'password'
 
   user = User.create!(first_name: first_name,
@@ -23,41 +25,31 @@
                       password: password)
 end
 
-
-
-8.times do |n|
-
+8.times do |_n|
   title = "Web Design with #{Faker::ProgrammingLanguage.name.titleize}"
-  description = Faker::Lorem.paragraph(sentence_count: 2)
+  description = Faker::Lorem.paragraph(sentence_count: 10)
   project_cats = ProjectCategory.create!(title: title,
-                                description: description,
-                              )
-
+                                         description: description)
 end
 
-
-
-100.times do |n|
-
-  title = Faker::Book.title[0..50].titleize
-  description = Faker::Lorem.paragraph(sentence_count: 10)
+100.times do |_n|
+  title = Faker::Book.title[4..50].titleize
+  description = Faker::Lorem.paragraph(sentence_count: 100)
   amount = Faker::Number.number(digits: 4)
-  owned_user_id = Faker::Number.within(range: 1..5)
-  claimed_user_id = Faker::Number.within(range: 6..10)
+  owned_user_id = Faker::Number.within(range: 7..10)
+  claimed_user_id = nil
   claimed = false
   completed = false
   attachment_url = nil
   project_category_id = Faker::Number.within(range: 1..8)
 
   projects = Project.create!(title: title,
-                              description: description,
-                              amount: amount,
-                              owned_user_id: owned_user_id,
-                              claimed_user_id: claimed_user_id,
-                              claimed: claimed,
-                              completed: completed,
-                              attachment_url: attachment_url,
-                              project_category_id: project_category_id
-                              )
-
+                             description: description,
+                             amount: amount,
+                             owned_user_id: owned_user_id,
+                             claimed_user_id: claimed_user_id,
+                             claimed: claimed,
+                             completed: completed,
+                             attachment_url: attachment_url,
+                             project_category_id: project_category_id)
 end
