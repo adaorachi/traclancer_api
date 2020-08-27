d0105fd2c44dc9ef7e6dd9ab08bb8c6de1e47b7d
MaryAnn <annychuks07@gmail.com>
2020-08-27 11:25:44 +0100

    fix production routing path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://tracker-app.herokuapp.com"
+    origins "https://blooming-refuge-37740.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
