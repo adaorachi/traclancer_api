13ba6e49bca286fdb49cdb319828977aed3b5667
MaryAnn <annychuks07@gmail.com>
2020-08-27 11:58:54 +0100

    deploy app to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://blooming-refuge-37740.herokuapp.com"
+    origins "https://frozen-depths-34629.herokuapp.com/"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "blooming-refuge-37740.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_tracker_app", domain: "frozen-depths-34629.herokuapp.com"
 
 else
   Rails.application.config.session_store :cookie_store, key: "_tracker_app"
