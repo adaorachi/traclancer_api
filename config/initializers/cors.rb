7e1447c291294211bb56008699b2f59dfd000316
MaryAnn <annychuks07@gmail.com>
2020-09-03 09:44:33 +0100

    fix CORS issue in cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://enigmatic-retreat-81755-react.herokuapp.com'
+    origins 'https://traclancer.netlify.app'
     resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
