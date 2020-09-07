1018f23f5215346d8b8a9005bef4859bda3fd193
MaryAnn <annychuks07@gmail.com>
2020-09-07 03:40:21 +0100

    modify seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   enum status: %i[freelancer client]
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :first_name, presence: true, length: { in: 3..50 }
-  validates :last_name, presence: true, length: { in: 3..50 }
-  validates :username, presence: true, length: { in: 3..50 },
+  validates :first_name, presence: true, length: { in: 1..20 }
+  validates :last_name, presence: true, length: { in: 1..20 }
+  validates :username, presence: true, length: { in: 1..20 },
                        uniqueness: { case_sensitive: false }
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false },
